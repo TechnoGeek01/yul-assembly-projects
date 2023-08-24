d2112b1a3802c8447699cbb383a5b54d01299ca3
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-24 17:38:55 +0530

    added a project with storage offsets, bit shifting and masking. Need to add some more functions

---
+++ YulStorageOffsetsAndBitShifting.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+contract YulStorageOffsetsAndBitShifting {
+    uint128 public A = 1;
+    uint96 public B = 2;
+    uint16 public C = 3;
+    uint8 public D = 4;
+
+    function readValueBySlot(uint slot) external view returns (uint value) {
+        assembly {
+            value := sload(slot)
+        }
+    }
+
+    function getOffsetOfC() external pure returns (uint slot, uint offset) {
+        assembly {
+            slot := C.slot
+            offset := C.offset
+        }
+    }
+
+    function readC() external view returns (uint c) {
+        assembly {
+            let value := sload(C.slot)
+            let shifted := shr(mul(C.offset, 8), value)
+
+            c := and(0xffff, shifted)
+        }
+    }
+
+    // dangerous to do
+    function writeValueToASlot(uint slot, uint value) external {
+        assembly {
+            sstore(slot, value)
+        }
+    }
+}
